597057de5f8958143fa16084ecb177e835f545a2
Mark Stramaglia <markstramaglia@gmail.com>
2018-09-08 15:59:27 -0700

    Update and rename java_to_pdf_converter.ps1 to Java-To-PDF.ps1

---
+++ Java-To-PDF.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    Program to extract and convert .java source files to PDF.
+.DESCRIPTION
+    Program to extract .java source files from Canvas LMS Homework
+    submission ZIP archives, and then convert those .java source files
+    into combined PDF files per student, so that they be graded and
+    annotated with notes in Adobe Acrobat.
+.NOTES
+    File Name  : Java-To-PDF.ps1
+    Author     : Mark Stramaglia <markstramaglia@gmail.com>
+.LINK
+    https://github.com/markstramaglia/java_homework_to_pdf
+#>
