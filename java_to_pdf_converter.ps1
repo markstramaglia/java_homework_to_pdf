4ee7ac0da5f9b3e0693d971cd5f13a4647e15627
Mark Stramaglia <markstramaglia@gmail.com>
2018-09-08 15:49:56 -0700

    Create java_to_pdf_converter.ps1

---
+++ java_to_pdf_converter.ps1
@@ -0,0 +1,14 @@
+<# ##################################################################
+
+          Script: java_to_pdf_converter.ps1
+          Author: Mark Stramaglia (markstramaglia@gmail.com)
+            Date: September 8, 2018
+
+     Description: Program to extract .java source files from
+                  Canvas LMS Homework submission ZIP archives,
+                  and then convert those .java source files
+                  into combined PDF files per student, so that
+                  they be graded and annotated with notes in
+                  Adobe Acrobat.
+
+## ############################################################### #>
