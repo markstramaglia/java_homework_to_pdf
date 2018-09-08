e071850114f04132c2bda258e7915f6ec500e8e2
Mark Stramaglia <markstramaglia@gmail.com>
2018-09-08 16:53:58 -0700

    Update Convert-Java-To-PDF.ps1

--- Convert-Java-To-PDF.ps1
+++ Convert-Java-To-PDF.ps1
@@ -12,3 +12,37 @@
 .LINK
     https://github.com/markstramaglia/java_homework_to_pdf
 #>
+
+
+# PROMPT USER FOR FILE PATH
+$SubmissionsFileName = "submissions.zip"
+
+Write-Host ""
+$SubmissionsFilePath = Read-Host -Prompt "INPUT: Please provide full path to submissions.zip file"
+Write-Host ""
+
+If($SubmissionsFilePath.Substring($SubmissionsFilePath.Length-1,1) -ne "\")
+{
+    $SubmissionsFilePath = $SubmissionsFilePath + "\"
+}
+
+# VERIFY EXISTENCE OF ZIP FILE
+$SubmissionsFullFilePath = $SubmissionsFilePath + $SubmissionsFileName
+Write-Host " INFO: Checking for ZIP archive: $SubmissionsFullFilePath"
+$FileExists = Test-Path -Path $SubmissionsFullFilePath -PathType leaf
+If($FileExists)
+{
+    Write-Host " INFO: Found submissions.zip file!"
+}
+Else
+{
+    Write-Host "ERROR: Did not find submissions.zip file.  Please verify that file path is correct."
+    Write-Host "ERROR: Exiting program."
+    Exit
+}
+
+
+# EXTRACT ZIP ARCHIVE
+$SubmissionsDestinationPath = $SubmissionsFilePath + "extracted_submissions\"
+Write-Host " INFO: Expanding submissions.zip archive..."
+Expand-Archive -LiteralPath $SubmissionsFullFilePath -DestinationPath $SubmissionsDestinationPath
