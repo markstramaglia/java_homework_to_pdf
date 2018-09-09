2a48c2756e5c02025ced3bd696265c451e260844
Mark Stramaglia <markstramaglia@gmail.com>
2018-09-09 15:30:32 -0700

    Version 1.0 Commit

--- script/Convert-Java-To-PDF.ps1
+++ script/Convert-Java-To-PDF.ps1
@@ -7,31 +7,34 @@
     into combined PDF files per student, so that they be graded and
     annotated with notes in Adobe Acrobat.
 .NOTES
-    File Name     : Convert-Java-To-PDF.ps1
-    Author        : Mark Stramaglia <markstramaglia@gmail.com>
-    PDF Converter : iTextSharp: https://github.com/itext/itextsharp
+    Version:       1.0
+    Last Updated:  09-SEP-2018
+    File Name:     Convert-Java-To-PDF.ps1
+    Author:        Mark Stramaglia <markstramaglia@gmail.com>
+    PDF Library:   iTextSharp: https://github.com/itext/itextsharp
 .LINK
     https://github.com/markstramaglia/java_homework_to_pdf
+.HISTORY
+    Version 1.0 (09-SEP-2018): Initial working version.
 #>
 
-# IMPORT PDF CONVERSION MODULE
-Add-Type -Path "$PSScriptRoot\itextsharp.dll"
-
-# PROMPT USER FOR FILE PATH
+# -------------------------------------------------------------------
+# Prompt for the Path to the "submissions.zip" file
+# -------------------------------------------------------------------
 $SubmissionsFileName = "submissions.zip"
-
 Write-Host ""
 $SubmissionsFilePath = Read-Host -Prompt "INPUT: Please provide full path to submissions.zip file"
 Write-Host ""
 $FolderSuffix = Read-Host -Prompt "INPUT: Please provide folder suffix to use"
 Write-Host ""
-
 If($SubmissionsFilePath.Substring($SubmissionsFilePath.Length-1,1) -ne "\")
 {
     $SubmissionsFilePath = $SubmissionsFilePath + "\"
 }
 
-# VERIFY EXISTENCE OF ZIP FILE
+# -------------------------------------------------------------------
+# Verify the existence of the zip file at the given path
+# -------------------------------------------------------------------
 $SubmissionsFullFilePath = $SubmissionsFilePath + $SubmissionsFileName
 Write-Host " INFO: Checking for ZIP archive: $SubmissionsFullFilePath"
 $FileExists = Test-Path -Path $SubmissionsFullFilePath -PathType leaf
@@ -46,19 +49,25 @@ Else
     Exit
 }
 
-# EXTRACT ZIP ARCHIVE
+# -------------------------------------------------------------------
+# Extract the main "submissions.zip" archive
+# -------------------------------------------------------------------
 $SubmissionsDestinationPath = $SubmissionsFilePath + "extracted_submissions\"
 Write-Host " INFO: Expanding submissions.zip archive..."
 Expand-Archive -LiteralPath $SubmissionsFullFilePath -DestinationPath $SubmissionsDestinationPath
 
-# REPLACE SPACES WITH UNDERSCORES IN ZIP ARCHIVE FILE NAMES
+# -------------------------------------------------------------------
+# Clean up file names for the individual student zip files
+# -------------------------------------------------------------------
 $archives = Get-ChildItem -Path ($SubmissionsDestinationPath + "*") -Include *.zip
 ForEach($archive In $archives)
 {
     Rename-Item -Path $archive.FullName –NewName ($archive.Name –replace " ", "_")
 }
 
-# EXPAND STUDENT SUBMISSION ZIP ARCHIVES
+# -------------------------------------------------------------------
+# Extract each student zip archive and convert .java files to PDF
+# -------------------------------------------------------------------
 $archives = Get-ChildItem -Path ($SubmissionsDestinationPath + "*") -Include *.zip
 ForEach($archive In $archives)
 {
@@ -81,30 +90,43 @@ ForEach($archive In $archives)
     # Delete the original ZIP file
     Remove-Item -Path $archive.FullName
 
-    # Find all the .java source files in student folder
+    # Create PDF from Java File
+    $PDFOutputFilePath = $DestinationPath + ($archiveFolderName.Substring(0, $archiveFolderName.Length-1) + ".pdf")
+    Write-Host " INFO: Creating PDF: " $PDFOutputFilePath
+    [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\itextsharp.dll")
+    $doc = New-Object iTextSharp.text.Document
+    $fileStream = New-Object IO.FileStream("$PDFOutputFilePath", [System.IO.FileMode]::Create)
+    [iTextSharp.text.pdf.PdfWriter]::GetInstance($doc, $filestream)
+
+    #Set font to use in PDF
+    [iTextSharp.text.FontFactory]::RegisterDirectories()
+    $arial = [iTextSharp.text.FontFactory]::GetFont("arial", 10)
+
+    # Open PDF for Writing
+    $doc.open()
+
+    # Find all the .java source files in student folder and write contents to PDF
     $JavaFiles = Get-ChildItem -Path $DestinationPath -Recurse -Include *.java -Exclude .*
-    ForEach($JavaFile In $JavaFiles)
+    If($JavaFiles.Count -gt 0)
     {
-        Write-Host " INFO:   Java File:" $JavaFile.FullName
-
-        # Create PDF from Java File
-        $PDFOutputFilePath = $JavaFile.FullName -replace "\.java", ".pdf"
-        Write-Host " INFO: Creating PDF: " $PDFOutputFilePath
-        [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\itextsharp.dll")
-
-        $doc = New-Object iTextSharp.text.Document
-        $fileStream = New-Object IO.FileStream("$PDFOutputFilePath", [System.IO.FileMode]::Create)
-        [iTextSharp.text.pdf.PdfWriter]::GetInstance($doc, $filestream)
-
-        #iTextSharp provides a class to work with fonts, but first we have to register them:
-        [iTextSharp.text.FontFactory]::RegisterDirectories()
-        $arial = [iTextSharp.text.FontFactory]::GetFont("arial", 10)
-
-        $paragraph = New-Object iTextSharp.text.Paragraph
-
-        $paragraph.add((gc $JavaFile.FullName|%{"$_`n"})) | Out-Null
-        $doc.open()
-        $doc.add($paragraph) | Out-Null
-        $doc.close()
+        ForEach($JavaFile In $JavaFiles)
+        {
+            Write-Host " INFO:   Java File:" $JavaFile.FullName
+            $paragraph = New-Object iTextSharp.text.Paragraph
+            $paragraph.add((gc $JavaFile.FullName|%{"$_`n"})) | Out-Null
+            $doc.NewPage()
+            $doc.add($paragraph) | Out-Null
+        
+        }
     }
+    Else
+    {
+        Write-Host " INFO: No Java Source files submitted by student!"
+        $paragraph = New-Object iTextSharp.text.Paragraph
+        $paragraph.add("No Java source code files were turned in.") | Out-Null
+        $doc.add($paragraph) | Out-Null
+    }
+
+    # Close PDF
+    $doc.close()
 }
